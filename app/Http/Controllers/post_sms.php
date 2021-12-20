ed617075d9f9ddb297b3b0bdba28e6f8aac2e529
ramzi allia <53237827+allia5@users.noreply.github.com>
2021-12-20 22:50:57 +0100

    Project_Send_Auto_Sms_Client

---
+++ app/Http/Controllers/post_sms.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use MessageBird\Client;
+use MessageBird\Objects\Message;
+use Symfony\Component\VarDumper\VarDumper;
+
+class post_sms extends Controller
+{
+    public function post_sms1($numm,$mesasge_)
+    {
+        /*require_once 'vendor/autoload.php';*/
+        $messagebird = new  Client('bZuMLHUpFHiJ7YVJ2fwb4MmLp');
+        $message = new Message;
+        $message->originator = $numm;
+        $message->recipients = [ $numm ];
+        $message->body = 'message:'.$mesasge_;
+        $response = $messagebird->messages->create($message);
+        var_dump($response);
+        
+    }
+}
